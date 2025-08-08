4f14eda2dd980a54fe0f79a04a982ff986780ab3
Mayank Mohit Agarwal <mayankmohitagarwal7@gmail.com>
2025-08-09 02:00:28 +0530

    Bugs fixed for production

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./css/App.css";
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import {MovieProvider} from './contexts/MovieContext'
 import { Routes, Route } from "react-router-dom";
 
